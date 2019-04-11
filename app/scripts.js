68a8dc40646d7cc76466842c606667fe144d017d
Adilson G Perinei <adilson.perinei@yahoo.com>
2019-04-11 16:27:27 -0400

    release_date

--- app/scripts.js
+++ app/scripts.js
@@ -17,7 +17,7 @@ request.onload = function () {
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = movie.release_date;
 
       const p = document.createElement('p');
       movie.description = movie.description.substring(0, 300);
