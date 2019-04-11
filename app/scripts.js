7878a898b65b93ef37daf11b81aaed2f74ff558a
Adilson G Perinei <adilson.perinei@yahoo.com>
2019-04-11 16:25:29 -0400

    o

--- app/scripts.js
+++ app/scripts.js
@@ -1,16 +1,12 @@
 const app = document.getElementById('root');
 
-const logo = document.createElement('img');
-logo.src = 'logo.png';
-
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
-app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://zjvba121aj.execute-api.us-east-1.amazonaws.com/dev', true);
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
@@ -21,11 +17,11 @@ request.onload = function () {
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.Serial;
+      h1.textContent = movie.title;
 
       const p = document.createElement('p');
-      movie.status = movie.status.substring(0, 300);
-      p.textContent = `${movie.status}...`;
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
