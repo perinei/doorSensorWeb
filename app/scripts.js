fcebe4544c2c8bc50962967b21b06b3e79461ff1
Adilson G Perinei <adilson.perinei@yahoo.com>
2019-04-11 15:50:38 -0400

    my api

--- app/scripts.js
+++ app/scripts.js
@@ -10,7 +10,7 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'https://zjvba121aj.execute-api.us-east-1.amazonaws.com/dev', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
@@ -21,11 +21,11 @@ request.onload = function () {
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = movie.Serial;
 
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      movie.status = movie.status.substring(0, 300);
+      p.textContent = `${movie.status}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
