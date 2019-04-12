b926dea5043fa7c29305641aa72cde467737bd3b
Adilson G Perinei <adilson.perinei@yahoo.com>
2019-04-12 09:58:43 -0400

    convert date

--- app/scripts.js
+++ app/scripts.js
@@ -19,8 +19,10 @@ request.onload = function () {
       const h1 = document.createElement('h1');
       h1.textContent = item.Serial;
 
+      var when = await convert(item.date_time);
+
       const p = document.createElement('p');
-      p.textContent = `${item.status} at ${item.date_time}`;
+      p.textContent = `${item.status} at ${when}`;
 
       container.appendChild(card);
       card.appendChild(h1);
@@ -36,3 +38,35 @@ request.onload = function () {
 
 request.send();
 // end
+
+async function convert(unixtimestamp){
+ 
+  // Months array
+  var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+ 
+  // Convert timestamp to milliseconds
+  var date = new Date(unixtimestamp*1000);
+ 
+  // Year
+  var year = date.getFullYear();
+ 
+  // Month
+  var month = months_arr[date.getMonth()];
+ 
+  // Day
+  var day = date.getDate();
+ 
+  // Hours
+  var hours = date.getHours();
+ 
+  // Minutes
+  var minutes = "0" + date.getMinutes();
+ 
+  // Seconds
+  var seconds = "0" + date.getSeconds();
+ 
+  // Display date time in MM-dd-yyyy h:m:s format
+  var convdataTime = month+'-'+day+'-'+year+' '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  return convdataTime;
+  
+ }
